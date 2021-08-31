7c63b499e14aa009d74caa1501579e429f362334
dhan93 <r.dhan93@gmail.com>
2021-09-01 04:15:01 +0700

    change link to dropbox

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -13,22 +13,22 @@
     <x-link-list href="https://youtu.be/hOQR_GsQ8e4" target="_blank">
       Perkenalan Manajemen Sekolah 7 Perempuan <span class="icon-launch"></span>
     </x-link-list>
-    <x-link-list href="" target="_blank">
+    <x-link-list href="https://dl.dropboxusercontent.com/s/j8t9jqg9w6m30yo/Silabus%207PS4%20rev.1.pdf?dl=0" target="_blank">
       Silabus Sekolah 7 Perempuan <span class="icon-launch"></span>
     </x-link-list>
-    <x-link-list href="https://drive.google.com/file/d/1VGKdNMN40pPmes5ytc9TTL8p0veHi_wH/view?usp=sharing" target="_blank">
+    <x-link-list href="https://dl.dropboxusercontent.com/s/6dfggt0prek4dj5/SOP%207PS4.pdf?dl=0" target="_blank">
       SOP Sekolah 7 Perempuan <span class="icon-launch"></span>
     </x-link-list>
-    <x-link-list href="https://drive.google.com/file/d/1vNe1gcJ7DKS7IffH1eAd45-QJLDrKFPS/view?usp=sharing" target="_blank">
+    <x-link-list href="https://dl.dropboxusercontent.com/s/yey7k767qrox8h3/FAQ%207PS4.pdf?dl=0" target="_blank">
       FAQ Sekolah 7 Perempuan <span class="icon-launch"></span>
     </x-link-list>
-    <x-link-list href="" target="_blank">
+    <x-link-list href="https://dl.dropboxusercontent.com/s/5kcr0kem3mewocc/Jurnal%207PS4.pdf?dl=0" target="_blank">
       Jurnal Sekolah 7 Perempuan <span class="icon-launch"></span>
     </x-link-list>
     <x-link-list href="{{route('guide.index')}}">
       Kumpulan Tutorial Sekolah 7 Perempuan
     </x-link-list>
-    <x-link-list href="https://drive.google.com/file/d/1jwsi723wcMZOBHXdq4UbZMsCv659V7tF/view?usp=sharing" target="_blank">
+    <x-link-list href="https://dl.dropboxusercontent.com/s/apgwuqpzlj6u1od/Dzikir%20Pagi%20Petang%20S7P.pdf?dl=0" target="_blank">
       Dzikir Pagi dan Petang <span class="icon-launch"></span>
     </x-link-list>    
   </x-card>
