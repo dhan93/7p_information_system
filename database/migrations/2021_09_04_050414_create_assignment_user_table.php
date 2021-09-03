0bc8d32de590353407b9896aa18468a0b4b2a03a
dhan93 <r.dhan93@gmail.com>
2021-09-04 06:02:38 +0700

    add assignment table

---
+++ database/migrations/2021_09_04_050414_create_assignment_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignment_user', function (Blueprint $table) {
+            $table->string('id', 12)->unique()->comment('assignmentId_userId');
+            $table->unsignedInteger('assignment_id');
+            $table->foreignId('user_id')->constrained();
+
+            $table->foreign('assignment_id')->references('id')->on('assignments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignment_user');
+    }
+}
